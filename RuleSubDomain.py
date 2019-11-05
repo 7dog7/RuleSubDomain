c0bb75a44b3905165de9985716465604ee6df6ed
专注无用工具开发 <50615524+7dog7@users.noreply.github.com>
2019-11-05 14:15:12 +0800

    Add files via upload

---
+++ RuleSubDomain.py
@@ -0,0 +1,46 @@
+#!./usr/bin/env.python
+# .-*- coding: utf-8 -*-
+# ._author_.=."me7dog7"
+
+ruleList = [
+    '{sub}{domain}',
+    '{domain}{sub}',
+    '{sub}{rule}{domain}',
+    '{domain}{rule}{sub}',
+    '{domain}.{sub}',
+    '{sub}.{domain}'
+]
+ruleData = [
+    '-',
+    '.',
+    '_'
+]
+
+def main(domain, lines):
+    with open('subname.txt') as inFile:
+        for line in inFile.xreadlines():
+            sub = line.strip()
+            if not sub or sub in lines:
+                continue
+            for rule in ruleList:
+                for i in ruleData:
+                    rule = rule.replace('{sub}', sub)
+                    rule = rule.replace('{domain}', domain)
+                    rule = rule.replace('{rule}', i)
+                lines.add(rule)
+    return lines
+
+
+if __name__ == '__main__':
+    domainList = set()
+    lines = set()
+    with open('domain.txt') as inFile:
+        for line in inFile.xreadlines():
+            domain = line.strip()
+            domainList.add(domain)
+
+    for i in domainList:
+        main(i[:i.rfind(".", 0, i.rfind('.'))], lines)
+    with open(r'ruleSub.txt', 'w') as ff:
+        for i in lines:
+            ff.write(i + '\n')
